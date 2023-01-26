32b94da3b832c4af1a47a2f86b8cf4cd419666f1
Erika Sha <erikaasha05@gmail.com>
2023-01-25 19:55:48 -0500

    updates for eb

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+from flask_cors import CORS
 import os
 
 db = SQLAlchemy()
 migrate = Migrate(compare_type=True)
+load_dotenv()
+
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -18,12 +22,13 @@ def create_app(test_config=None):
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_TEST_DATABASE_URI")
 
-    db.init_app(app)
-    migrate.init_app(app, db)
-
     # Import models
     from app.models.contact import Contact
     from app.models.reminder import Reminder
+
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # Register Blueprints
     from .contact_routes import contacts_bp
@@ -31,4 +36,5 @@ def create_app(test_config=None):
     app.register_blueprint(contacts_bp)
     app.register_blueprint(reminders_bp)
 
+    CORS(app)
     return app
