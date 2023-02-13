26d2a234ae728cbdb4aa1590a7e595d455a56baf
Erika Sha <erikaasha05@gmail.com>
2023-02-13 02:11:58 -0500

    added Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -17,24 +17,24 @@ def create_app(test_config=None):
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=2)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    # if test_config is None:
-    #     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    #         "SQLALCHEMY_DATABASE_URI")
+    if test_config is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
     # else:
     #     app.config["TESTING"] = True
     #     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
     #         "SQLALCHEMY_TEST_DATABASE_URI")
 
-    if "RDS_DB_NAME" in os.environ:
-        app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://{username}:{password}@{host}:{port}/{database}".format(
-        username=os.environ['RDS_USERNAME'],
-        password=os.environ['RDS_PASSWORD'],
-        host=os.environ['RDS_HOSTNAME'],
-        port=os.environ['RDS_PORT'],
-        database=os.environ['RDS_DB_NAME'],
-    )
-    else:
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    # if "RDS_DB_NAME" in os.environ:
+    #     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://{username}:{password}@{host}:{port}/{database}".format(
+    #     username=os.environ['RDS_USERNAME'],
+    #     password=os.environ['RDS_PASSWORD'],
+    #     host=os.environ['RDS_HOSTNAME'],
+    #     port=os.environ['RDS_PORT'],
+    #     database=os.environ['RDS_DB_NAME'],
+    # )
+    # else:
+    #     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
     JWTManager(app)
 
